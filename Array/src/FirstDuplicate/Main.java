f74d9f5e2528870ec5f2989dd404fc54c7f42a5f
Dung <99176221+hdng-88@users.noreply.github.com>
2023-07-09 18:22:09 +0000

    add code

--- Array/src/FirstDuplicate/Main.java
+++ Array/src/FirstDuplicate/Main.java
@@ -1,9 +1,43 @@
 package FirstDuplicate;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
 
-    
+    static int T, n;
+    static boolean check;
+    static int[] a, count;
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("/workspaces/STP/Array/src/FirstDuplicate/input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            int n = sc.nextInt();
+            a = new int[n];
+            count = new int[n+1];
+
+            check = false;
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                if(++count[a[i]] == 2){
+                    check = true;
+                    System.out.println("#" + tc + " " + a[i]);
+                    break;
+                }
+            }
+
+            if(!check){
+                System.out.println("#" + tc + " -1");
+            }
+        }
+    }
 }
